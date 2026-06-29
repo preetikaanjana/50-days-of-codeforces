df7802761ffed5db6ca2f16358bad775f8a6cc91
preetikaanjana <preetikaanjana@gmail.com>
2026-06-29 23:53:16 +0530

    feat: Day 04 Domino piling complete

---
+++ Phase-1-Basics/A_Domino_piling.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int m, n;
+    cin>>m>>n;
+    int total_square= (m*n)/2;
+    cout<<total_square<<"\n";
+    return 0;
+   
+}
